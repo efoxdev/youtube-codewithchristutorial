fa384e790cf9cd954bfe62c2feefa86ac06e9410
Ethan Fox <efox@solutioninnovators.com>
2020-06-24 12:33:18 -0400

    initial xcode project creation. BOOYYAAAAA

---
+++ Youtube-codewithchristutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube-codewithchristutorial
+//
+//  Created by Ethan Fox on 6/11/20.
+//  Copyright © 2020 Ethan Fox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
